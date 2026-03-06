74c349bdf002a8019cf58d65e966860e2736bd21
CloneWave <adiranamagar@gmail.com>
2026-03-06 09:53:01 +0545

    implemented custom style in tailwindv4

---
+++ 04-Tailwind-CSS/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './input.css'
+const App = () => {
+  return (
+<>
+    <div className='bg-amber-300 min-h-20 min-w-50'>
+        Tailwind
+    </div>
+    <div className='bg-mint-500 min-h-20 min-w-50'>
+        Tailwind
+    </div>
+</>
+  )
+}
+
+export default App
